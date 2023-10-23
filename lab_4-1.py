041e120cbd0bf3e7686c8864e339b0ab26118ff1
p24lohara <p24lohara@fairfieldprep.org>
2023-10-22 20:51:20 -0400

    Lab 4-1 Submission

--- lab_4-1.py
+++ lab_4-1.py
@@ -9,3 +9,17 @@ Create a new variable for the user's full name and set it equal to the first nam
 Is there an issue with this output? How could we fix that?
 
 """
+# Liam O'Hara
+
+first_name = input("Please enter your first name:")
+# Prompt user for first name - Liam O'Hara
+
+last_name = input ("Please enter your last name:") 
+# Prompt user for last name - O'Hara
+
+full_name = first_name + last_name  
+# Combine both variables into one full name - Liam O'Hara 
+print(full_name)
+
+# The output does not have a space between the first and last name.
+# We could fix that by including a space between the first_name and last_name variables.
